5e357ca54c74a78168cedcc9467a5c5e732e6a29
KP824 <112226540+KP824@users.noreply.github.com>
2023-08-25 10:21:14 -0700

    React router setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-
 import App from "./components/App";
 
 function Routes() {
   return <App />;
 }
 
-const root = ReactDOM.createRoot(document.getElementById("app"));
-root.render(<Routes />);
+const root = ReactDOM.createRoot(document.getElementById("app"));root.render(<Routes />);
+
--- src/store.ts
+++ src/store.ts
@@ -1,28 +1,28 @@
-import { create } from "zustand";
+// import { create } from "zustand";
 
-// import types
+// // import types
 
-// interface for store
+// // interface for store
 
-interface StoreState {
+// interface StoreState {
 
-}
+// }
 
-const useStore = create<StoreState>((set) => {
-  // set initial state here
+// const useStore = create<StoreState>((set) => {
+//   // set initial state here
   
 
-  // login functionality
+//   // login functionality
 
-  // inputs/answers from user?
+//   // inputs/answers from user?
 
-  // feed of trips from all users
-    // how do we grab all of the trips from the database?
-    // use axios to fetch from the database
+//   // feed of trips from all users
+//     // how do we grab all of the trips from the database?
+//     // use axios to fetch from the database
 
-  // feed of saved trips from logged in user
-    // how do we grab all of the trips from logged in user?
-    // use axios to fetch form the database
+//   // feed of saved trips from logged in user
+//     // how do we grab all of the trips from logged in user?
+//     // use axios to fetch form the database
 
-  // itinerary generated?
-})
+//   // itinerary generated?
+// })
