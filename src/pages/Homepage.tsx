07b7a3863a1c67a81b3ab4325936abc479272155
KP824 <112226540+KP824@users.noreply.github.com>
2023-08-25 22:24:04 -0700

    Onboard styled components and global standard css

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { css, styled } from 'styled-components';
+
+// title component to render h1 tag
+const Title = styled.h1`
+font-size: 1.5em;
+text-align: center;
+color: #BF4F74;
+`;
+
+// wrapper component that'll render a <section> tag with some styles
+const Wrapper = styled.section`
+padding: 4em;
+background: papayawhip;
+`;
 
 const Homepage = () => {
+
   return (
     <>
-      <h1>Welcome to your next adventure</h1>
+      <h1 className="testing">Welcome to your next adventure</h1>
+      <Wrapper>
+        <Title>
+          Chegg it out, my first styled component!
+        </Title>
+      </Wrapper>
     </>
   )
 }
