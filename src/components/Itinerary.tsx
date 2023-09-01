15ead2084900b3c817d77327931dfdd2418a1094
KP824 <112226540+KP824@users.noreply.github.com>
2023-08-31 20:10:41 -0700

    hard coded image for pexel

--- src/components/Itinerary.tsx
+++ src/components/Itinerary.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 const ItineraryContainer = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
   border-radius: 20px;
   border: 1px solid black;
@@ -22,11 +22,41 @@ const ItineraryContainer = styled.div`
   }
 `;
 
+const TripImage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 250px;
+  position: relative;
+`;
+
+const PexelImg = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+
+
+
+
 const Itinerary = () => {
 
   return (
     <ItineraryContainer>
-      <h1>Hi from inside Itinerary container</h1>
+      
+      <TripImage>
+        <PexelImg src="https://images.pexels.com/photos/460672/pexels-photo-460672.jpeg" alt="London Bridge"/>
+      </TripImage>
+
+      <h1>Your next vacation</h1>
+
+      
+
     </ItineraryContainer>
   )
 };
