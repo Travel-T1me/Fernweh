1c0be324b32a2cb738bc4f93352dd4ab620882eb
KP824 <112226540+KP824@users.noreply.github.com>
2023-10-25 15:12:35 -0700

    Added fernweh-favicon.png file to the assets folder for build

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
   },
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "public"),
     clean: true,
   },
   module: {
